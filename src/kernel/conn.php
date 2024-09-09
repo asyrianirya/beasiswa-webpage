c22a0fa02d586dcc2531bd094422f4df113e6042
Asyari <burhanudinasyari02@gmail.com>
2024-09-09 09:55:53 +0700

    added port option

--- src/kernel/conn.php
+++ src/kernel/conn.php
@@ -4,8 +4,9 @@ $hostname = "localhost";
 $username = "root";
 $password = "";
 $database = "beasiswa_webpage";
+$port = 3306;
 
-$conn = mysqli_connect($hostname, $username, $password, $database);
+$conn = mysqli_connect($hostname, $username, $password, $database, $port);
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
